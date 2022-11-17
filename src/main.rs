0d7f1c911c3119d12f2e4251c908698d2b9755aa
Bostjan Meglic <103102696+bmeglic@users.noreply.github.com>
2022-11-17 18:18:45 +0100

    [ph] Implement Protohackers problem 1

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use std::thread;
-use std::{net::{TcpListener, TcpStream, Shutdown}};
-use std::io::{Read, Write};
 use clap::Parser;
-
+use primes;
+use serde::Serialize;
+use serde_json::{Result, Value};
+use std::io::{BufRead, BufReader, BufWriter, Write};
+use std::net::{Shutdown, TcpListener, TcpStream};
+use std::thread;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -22,8 +24,7 @@ fn main() {
     let hostname = format!("{}:{}", args.host, args.port);
     println!("Will start listening on {}", hostname);
 
-    let listener = TcpListener::bind(hostname).
-        expect("Unable to bind to socket");
+    let listener = TcpListener::bind(hostname).expect("Unable to bind to socket");
 
     for stream in listener.incoming() {
         match stream {
@@ -39,22 +40,110 @@ fn main() {
     }
 }
 
-fn handle_client(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
-
+fn handle_client(stream: TcpStream) {
     println!("New connection: {}", stream.peer_addr().unwrap());
-    loop {
-        if let Ok(read) = stream.read(&mut buffer) {
-            if read == 0 {
+
+    let reader = BufReader::new(&stream);
+    let mut writer = BufWriter::new(&stream);
+
+    for line in reader.lines() {
+        dbg!(&line);
+
+        let line = &line.unwrap();
+        if let Ok((out, shutdown)) = parse_line(&line) {
+            //dbg!(&out);
+            println!("out: {}", &out);
+
+            if let Err(_) = writer.write(&out.as_bytes()) {
+                stream.shutdown(Shutdown::Both).unwrap();
                 break;
             }
-            if let Err(_) = stream.write(&buffer[0..read]) {
+
+            if let Err(_) = writer.write(b"\n") {
+                stream.shutdown(Shutdown::Both).unwrap();
+                break;
+            }
+            writer.flush().unwrap();
+
+            if shutdown == true {
                 stream.shutdown(Shutdown::Both).unwrap();
                 break;
             }
         } else {
+            stream.shutdown(Shutdown::Both).unwrap();
             break;
         }
     }
-    println!("Disconnected: {}", stream.peer_addr().unwrap())
+
+    println!("Client disconnected");
+    stream.shutdown(Shutdown::Write).unwrap_or(())
+}
+
+#[derive(Serialize)]
+struct PrimeResponse {
+    method: String,
+    prime: bool,
+}
+
+fn parse_line(input: &String) -> Result<(String, bool)> {
+    println!("inp: {}", &input);
+    let json: Result<Value> = serde_json::from_str(input);
+
+    let resp_ok = PrimeResponse {
+        method: "isPrime".to_owned(),
+        prime: true,
+    };
+    let resp_nok = PrimeResponse {
+        method: "isPrime".to_owned(),
+        prime: false,
+    };
+    let resp_malform = PrimeResponse {
+        method: "Error".to_owned(),
+        prime: false,
+    };
+
+    match json {
+        Ok(json) => {
+            //dbg!(&json);
+
+            let method = json.get("method");
+            let method = match method {
+                None => {
+                    return Ok((serde_json::to_string(&resp_malform).unwrap(), true));
+                }
+                Some(method) => method,
+            };
+
+            if method != "isPrime" {
+                return Ok((serde_json::to_string(&resp_malform).unwrap(), true));
+            }
+
+            let number = json.get("number");
+            let number = match number {
+                None => {
+                    return Ok((serde_json::to_string(&resp_malform).unwrap(), true));
+                }
+                Some(number) => number,
+            };
+
+            if number.is_number() != true {
+                return Ok((serde_json::to_string(&resp_malform).unwrap(), true));
+            }
+            if number.is_u64() != true {
+                return Ok((serde_json::to_string(&resp_nok).unwrap(), false));
+            }
+
+            let number = number.as_u64().unwrap();
+            if primes::is_prime(number) {
+                Ok((serde_json::to_string(&resp_ok).unwrap(), false))
+            } else {
+                Ok((serde_json::to_string(&resp_nok).unwrap(), false))
+            }
+        }
+        Err(e) => {
+            println!("Err: {}", e);
+            Ok((serde_json::to_string(&resp_malform).unwrap(), true))
+            //Err(e)
+        }
+    }
 }
